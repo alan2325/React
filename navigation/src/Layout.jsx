a6a96934fb206eeb35709ef48672b719c3923567
alan2325 <alalkumar06@gmail.com>
2024-10-16 10:12:24 +0530

    navigation

--- navigation/src/Layout.jsx
+++ navigation/src/Layout.jsx
@@ -3,19 +3,27 @@ import { Link ,Outlet} from "react-router-dom"
 function Layout(){
     return(
         <>
-        <nav>
-            <ul>
-                <li>
-                    <Link to="/">Home</Link>
-                </li>
-                <li>
-                    <Link to="/Contact">Contact</Link>
-                </li>
-                <li>
-                    <Link to="/About">About</Link>
-                </li>
-            </ul>
 
+        <nav className="navbar navbar-expand-sm bg-dark navbar-dark">
+        <div className="container-fluid">
+            <a className="navbar-brand" href="#">Logo</a>
+            <button className="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#collapsibleNavbar">
+            <span className="navbar-toggler-icon"></span>
+            </button>
+            <div className="collapse navbar-collapse" id="collapsibleNavbar">
+            <ul className="navbar-nav">
+                <li className="nav-item">
+                    <Link to="/" className="nav-link">Home</Link>
+                </li>
+                <li className="nav-item">
+                    <Link to="/Contact" className="nav-link">Contact</Link>
+                </li>
+                <li className="nav-item">
+                    <Link to="/About" className="nav-link">About</Link>
+                </li>    
+            </ul>
+            </div>
+        </div>
         </nav>
         <Outlet/>
         </>
